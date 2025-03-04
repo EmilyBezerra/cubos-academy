82727cbb815aafa2ad23d96cfdb4c268e0d15214
Emily Bezerra - Estudos <emily.bezerra9343@gmail.com>
2025-03-04 20:05:46 -0300

    Implementaçao do cadastrar postagem

--- modulo-backend/src/api-rest-orientacao-obejetos/controladores/PostControlador.ts
+++ modulo-backend/src/api-rest-orientacao-obejetos/controladores/PostControlador.ts
@@ -1,6 +1,7 @@
 import { autores, posts } from './../bancoDeDados';
 import { json, Request, Response } from "express";
 import Autor from "../modelos/Autor";
+import Post from '../modelos/Post';
 
 
 export default class PostControlador {
@@ -26,7 +27,31 @@ export default class PostControlador {
     };
 
    cadastrar(req: Request, res: Response) {
-   
+        const {titulo, descricao, autor_id} = req.body 
+
+        if(!titulo || !descricao || !autor_id) {
+            return res.status(400).json({
+                mensagem: "O título, descrição e id do autor é obrogatório"
+            });
+        };
+
+        const autor = autores.find((elemento) => {
+            return elemento.id === autor_id
+        });
+        
+        if (!autor) {
+            return res.status(404).json({
+                mensagem: 'O autor não foi encontrado'
+            });
+        };
+
+        const post = new Post({
+            titulo, 
+            descricao, 
+            autor 
+        });
+
+        return res.status(201).json(post)
     };
 
     editar(req: Request, res: Response) {
