343832978fdef8028705650d95a3b1d236c60b4b
Emily Bezerra - Estudos <emily.bezerra9343@gmail.com>
2025-03-06 23:33:57 -0300

    Aula de assincronismo, leitura e escrita de arquivos

--- modulo-backend/src/assincronismo/verificarIdade.ts
+++ modulo-backend/src/assincronismo/verificarIdade.ts
@@ -1,16 +1,38 @@
-function verificarIdade(idade) {
-    return new Promise((resolver, rejeitar) => {
-        if(idade > 17) {
-            resolver(`${idade} anos está liberado`)
-            return
-        }
+function verificaIdade(idade) {
+	return new Promise((resolver, rejeitar) => {
+		if (idade > 17) {
+			resolver(`${idade} anos está liberado`)
+			return
+		}
 
-        rejeitar(`${idade} anos não está liberado`)
-    })
+		rejeitar(`Você é de menor, pois tem ${idade} anos`)
+	})
 }
 
-verificarIdade(18).then((resultado) => {
-    console.log(resultado)
+console.log('Iniciou a fila da festa')
+
+verificaIdade(18).then((resultado) => {
+	console.log(resultado)
 }).catch((erro) => {
-    console.log(erro)
+	console.log(erro)
+}).finally(() => {
+	console.log('Este console.log foi executado ao final da resolução ou da rejeição da promessa')
 })
+
+verificaIdade(19).then((resultado) => {
+	console.log(resultado)
+}).catch((erro) => {
+	console.log(erro)
+}).finally(() => {
+	console.log('Este console.log foi executado ao final da resolução ou da rejeição da promessa')
+})
+
+verificaIdade(17).then((resultado) => {
+	console.log(resultado)
+}).catch((erro) => {
+	console.log(erro)
+}).finally(() => {
+	console.log('Este console.log foi executado ao final da resolução ou da rejeição da promessa')
+})
+
+console.log('Final da fila')
